39fa0775694f8eb6350a7157a896d62421ba45b1
William Strecker-Kellogg <willsk@bnl.gov>
2016-09-04 00:02:15 -0400

    Read cgroup procs/task data by assembling the path
    It's kind-of stupid the library doesn't provide a way to do this...

--- cgroup.c
+++ cgroup.c
@@ -7,6 +7,7 @@
 #include <errno.h>
 #include <linux/limits.h>
 #include <libcgroup.h>
+#include <assert.h>
 
 #define CONDOR_ROOT_GROUP "condor"
 
@@ -37,75 +38,114 @@ int add_group(struct cgroup_file_info *info, struct condor_group **grp)
 {
 	size_t root_len;
 	if(NULL != (*grp = find_group(info->path)))	{
-		printf("Found alread %s...\n", info->path);
+		printf("Found already %s...\n", info->path);
 		return 0;
 	} else {
-		printf("Adding group (%d) %s\n", n_groups + 1, info->path);
+		printf("Adding group (%d) %s -- %s\n", n_groups + 1, info->path, info->full_path);
 		if(NULL == (groups = realloc(groups,
 				(1 + n_groups) * sizeof(struct condor_group))))
 			exit(ENOMEM);
 		*grp = &groups[n_groups++];
 		root_len = strlen(info->full_path) - strlen(info->path);
+
 		/* Really!? This is the way to access a structure-member's size? */
-		strncpy((*grp)->name, info->path,
-				sizeof(((struct condor_group *)0)->name));
-		strncpy((*grp)->root_path, info->full_path, root_len);
-		*((*grp)->root_path + root_len + 1) = '\0';
+		strncpy((*grp)->name, info->path, sizeof(((struct condor_group *)0)->name));
+		strncpy((*grp)->root_path, info->full_path, sizeof(((struct condor_group *)0)->root_path));
+
+		*((*grp)->root_path + root_len) = '\0';
 		return 0;
 	}
 }
 
-/* Walk through the children of the "condor" cgroup under @controller
- *
- * return: 1 when there is more to go, 0 when done
+/* Walk through the children of the "condor" cgroup under one controller to
+ * get the names of the current slot-cgroups
  *
  * WARNING: This function uses static storage, not threadsafe!
  */
-int walk_condor_cgroups(const char *controller, struct cgroup_file_info *info)
+void get_condor_cgroups(void)
 {
 	static void *handle = NULL;
 	static int level = -1;
 	struct condor_group *g;
+	struct cgroup_file_info info;
+	const char *controller = "memory";
 	int ret;
 
 	if(handle == NULL)	{
 		ret = cgroup_walk_tree_begin(controller, CONDOR_ROOT_GROUP, 1,
-					     &handle, info, &level);
+					     &handle, &info, &level);
 		if (ret != 0) {
 			fprintf(stderr,
 				"Error walking controller: %s", controller);
 			exit(EXIT_FAILURE);
 		}
-		if(info->type == CGROUP_FILE_TYPE_DIR && info->depth == 1)
-			add_group(info, &g);
+		if(info.type == CGROUP_FILE_TYPE_DIR && info.depth == 1)
+			add_group(&info, &g);
 	}
-	while ((ret = cgroup_walk_tree_next(0, &handle, info, level)) != ECGEOF)	{
+	while ((ret = cgroup_walk_tree_next(0, &handle, &info, level)) != ECGEOF)	{
 		if (ret != 0) {
 			fprintf(stderr,
 				"Error walking controller: %s", controller);
 			exit(EXIT_FAILURE);
 		}
-		if(info->type == CGROUP_FILE_TYPE_DIR && info->depth == 1)
-			add_group(info, &g);
+		if(info.type == CGROUP_FILE_TYPE_DIR && info.depth == 1)
+			add_group(&info, &g);
 	}
 
 	cgroup_walk_tree_end(&handle);
 	handle = NULL;
-	return 0;
 }
 
 uint64_t parse_num(const char *str)
 {
-    char *p;
-    unsigned long int n;
+	char *p;
+	unsigned long int n;
 
 	errno = 0;
-    n = strtoul(str, &p, 10);
-    if(errno != 0) {
-        fprintf(stderr, "Error converting '%s' to integer\n", str);
-        exit(EXIT_FAILURE);
-    }
-    return (uint64_t)n;
+	n = strtoul(str, &p, 10);
+	if(errno != 0) {
+		fprintf(stderr, "Error converting '%s' to integer\n", str);
+		exit(EXIT_FAILURE);
+	}
+	return (uint64_t)n;
+}
+
+/* Count the newlines in @path, since PIDs are on-per-line */
+int read_num_tasks(const char *path)
+{
+	FILE *fp;
+	int n = 0;
+	char c;
+
+	if(NULL == (fp = fopen(path, "r")))	{
+		fprintf(stderr, "Error opening %s", path);
+		exit(EXIT_FAILURE);
+	}
+
+	while((c = fgetc(fp)) != EOF)	{
+		if(c == '\n')
+			++n;
+	}
+
+	fclose(fp);
+	return n;
+}
+
+void get_procs_in_group(struct condor_group *g)
+{
+	char path[sizeof(g->name) + sizeof(g->root_path) + 64];
+
+	assert(path != NULL);
+
+	snprintf(path, sizeof(path), "%s%s/cgroup.procs", g->root_path, g->name);
+	printf("Reading path: %s\n", path);
+	g->num_procs = read_num_tasks(path);
+
+	snprintf(path, sizeof(path), "%s%s/tasks", g->root_path, g->name);
+	printf("Reading path: %s\n", path);
+	g->num_tasks = read_num_tasks(path);
+
+	free(path);
 }
 
 void get_memory_stats(struct cgroup_controller *c, struct condor_group *g)
@@ -122,10 +162,7 @@ void get_memory_stats(struct cgroup_controller *c, struct condor_group *g)
 	if(rv != 0) { fprintf(stderr, "Error reading val\n"); exit(EXIT_FAILURE); }
 	g->swap_used = swap - rss;
 
-
-	// This doesn't work: need to read from file itself
-	//cgroup_get_value_string(c, "cgroup.procs", &s);
-
+	get_procs_in_group(g);
 }
 
 void get_statistics()
@@ -135,6 +172,8 @@ void get_statistics()
 	struct condor_group *g = NULL;
 	struct cgroup_controller *cont;
 	int ret;
+
+	assert(groups != NULL);
 
 	for(int i = 0; i < n_groups; i++)	{
 		g = &groups[i];
@@ -155,28 +194,15 @@ void get_statistics()
 	}
 }
 
-void get_condor_cgroups(void)
-{
-	struct cgroup_file_info info;
-	struct condor_group *g = NULL;
-
-	/* Walk the "memory" controller to get all cgroup names for now... */
-	while(walk_condor_cgroups("memory", &info) != 0)	{
-		add_group(&info, &g);
-		printf("%d: %s <-- %s (%d)\n", info.type, g->name, info.parent, info.depth);
-	}
-}
-
-
 void print_groups(void)
 {
 	struct condor_group *g = groups;
 
 	for(int i = 0; i < n_groups; i++, g = &groups[i])	{
-		printf("Group %d: %s\n", i, g->root_path);
+		printf("Group %d: %s\n", i, g->name);
 		printf("\tRSS: %lu\n\tSWAP: %lu\n", g->rss_used, g->swap_used);
+		printf("\tProcesses (threads): %d (%d)\n", g->num_procs, g->num_tasks);
 	}
-
 }
 
 int main(int argc, char *argv[])
